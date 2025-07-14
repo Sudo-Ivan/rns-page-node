eb27326763616ba65ae48a0b99d547348fd749f4
Sudo-Ivan <Sudo-Ivan@users.noreply.github.com>
2025-07-14 17:31:45 -0500

    Bump package version to 1.0.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="rns-page-node",
-    version="0.2.0",
+    version="1.0.0",
     author="Sudo-Ivan",
     author_email="",
     description="A simple way to serve pages and files over the Reticulum network.",
